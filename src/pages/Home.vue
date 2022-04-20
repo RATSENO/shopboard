db1a61a8d9726ac42125d0e145d15070f18ca533
RATSENO <onestar1014@gmail.com>
2022-04-20 16:59:55 +0900

    최초 커밋

---
+++ src/pages/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <div align="center">
+        <h1>welcome</h1>
+        <p>{{getTime()}}</p>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Home',
+    setup(){
+        const getTime=()=>{
+            return new Date().toString()
+        }
+
+        return {
+            getTime
+        }
+    }
+}
+</script>
